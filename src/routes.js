3a33ca67c84b70aca5b3a058caae3482e2774c82
rfsyhb <rafisyihab3@gmail.com>
2024-06-02 02:35:14 +0700

    update main routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,14 @@ const usersRoutes = require('./modules/users/usersRoutes');
 const todosRoutes = require('./modules/todos/todosRoutes');
 const goalsRoutes = require('./modules/goals/goalsRoutes');
 const notesRoutes = require('./modules/notes/notesRoutes');
+const discussionsRoutes = require('./modules/discussions/discussionsRoutes');
 
-const routes = [...usersRoutes, ...todosRoutes, ...goalsRoutes, ...notesRoutes];
+const routes = [
+  ...usersRoutes,
+  ...todosRoutes,
+  ...goalsRoutes,
+  ...notesRoutes,
+  ...discussionsRoutes,
+];
 
 module.exports = routes;
