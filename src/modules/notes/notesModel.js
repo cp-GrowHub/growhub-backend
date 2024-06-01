0b1591518a367c6240a5a8e2215f89d02b09d1fb
rfsyhb <rafisyihab3@gmail.com>
2024-06-01 21:52:33 +0700

    add notes model

---
+++ src/modules/notes/notesModel.js
@@ -0,0 +1,12 @@
+const notes = [
+  {
+    id: 'notes-1',
+    title: 'Hello, Notes!',
+    body: 'My new notes.',
+    archived: false,
+    createdAt: '2022-07-28T10:12:12.396Z',
+    ownerId: 'users-1',
+  },
+];
+
+module.exports = notes;
