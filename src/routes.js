3cd4bc57dd2e07ec493b4aac0f73557a18c90804
rfsyhb <rafisyihab3@gmail.com>
2024-06-01 22:18:48 +0700

    Add notes routes to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const usersRoutes = require('./modules/users/usersRoutes');
 const todosRoutes = require('./modules/todos/todosRoutes');
 const goalsRoutes = require('./modules/goals/goalsRoutes');
+const notesRoutes = require('./modules/notes/notesRoutes');
 
-const routes = [...usersRoutes, ...todosRoutes, ...goalsRoutes];
+const routes = [...usersRoutes, ...todosRoutes, ...goalsRoutes, ...notesRoutes];
 
 module.exports = routes;
